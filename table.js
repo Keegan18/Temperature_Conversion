4bdad93eb6dbc5522fa237c812e1f70de45a67cc
unknown <kolbeinsen.tafaoa@gmail.com>
2017-11-08 00:18:00 -1000

    Update 1

---
+++ table.js
@@ -0,0 +1,18 @@
+
+
+
+
+function toCelsius(num1, num2, num3){
+	console.log(num3 / (num1 * num2));
+}
+multiplyThenDivide(bango3, bango4, bango5);
+
+
+
+
+
+
+
+function toCelsius(f){
+	return (f*32) / (1.8)
+}
