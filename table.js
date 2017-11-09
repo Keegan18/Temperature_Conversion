c3e0b371adb1bd7d479288c4d846f0048058ffba
Keegan18 <kolbeinsen.tafaoa@gmail.com>
2017-11-08 14:23:00 -1000

    Update Final

--- table.js
+++ table.js
@@ -1,6 +1,6 @@
-
-
-
+var initTemperature = 20;
+var maxTemperature = 120;
+var incrementValue = 5;
 
 function toCelsius(num1, num2, num3){
 	console.log(num3 / (num1 * num2));
@@ -11,8 +11,3 @@ multiplyThenDivide(bango3, bango4, bango5);
 
 
 
-
-
-function toCelsius(f){
-	return (f*32) / (1.8)
-}
