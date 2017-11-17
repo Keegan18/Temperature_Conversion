ab340188678c549afce1954be941bc8d1316bb21
unknown <kolbeinsen.tafaoa@gmail.com>
2017-11-16 21:50:23 -1000

    Final 2

--- table.js
+++ table.js
@@ -1,13 +1,23 @@
-var initTemperature = 20;
-var maxTemperature = 120;
-var incrementValue = 5;
+var initTemp = 20;
+var maxTemp = 120;
+var incrValue = 5;
 
-function toCelsius(num1, num2, num3){
-	console.log(num3 / (num1 * num2));
+//States what each number is tied to//
+
+function toCelsius(F){
+	return (F * 32) / 1.8;
 }
-multiplyThenDivide(bango3, bango4, bango5);
+//Needed Function for Assignment//
 
+for (i = 20; i <= 120; i += 5){
+	console.log(toCelsius(i));
+		var table = document.getElementById("F to C");
+		var row = table.insertRow(-2);
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell (1);
+		  cell1.innerHTML = i;
+		  cell2.innerHTML = toCelsius(i);
+}
 
-
-
+//Creates the table rows & columns and inputs & outputs//
 
